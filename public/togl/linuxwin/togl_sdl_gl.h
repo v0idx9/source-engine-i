2863fd97715ad69babd2a0dc4a05909b30854976
v0idx9 <v0idx9@users.noreply.github.com>
2026-07-14 19:24:40 +0300

    Define APIENTRY and GLclampf for iOS OpenGLES3.

--- public/togl/linuxwin/togl_sdl_gl.h
+++ public/togl/linuxwin/togl_sdl_gl.h
@@ -5,6 +5,21 @@
 #if defined( IOS ) || defined( _IOS ) || defined( __IPHONEOS__ )
 #include <OpenGLES/ES3/gl.h>
 #include <OpenGLES/ES3/glext.h>
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+#ifndef _APIENTRY
+#define _APIENTRY APIENTRY
+#endif
+#ifndef GLAPI
+#define GLAPI extern
+#endif
+#ifndef GLclampf
+typedef float GLclampf;
+#endif
+#ifndef GLclampd
+typedef double GLclampd;
+#endif
 #ifndef GL_HALF_FLOAT
 #define GL_HALF_FLOAT 0x140B
 #endif
